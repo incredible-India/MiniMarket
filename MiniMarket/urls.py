1a96f844241b266d73ed799da7e95139d82f4e3a
Himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-08-31 14:25:07 +0530

    ini

--- MiniMarket/urls.py
+++ MiniMarket/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views as p_view
-from django.conf import settings
-from django.conf.urls import url
-from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +25,5 @@ urlpatterns = [
     path('user/',include('users.urls')),
     path('market/',include('marketAdmin.urls')),
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]
